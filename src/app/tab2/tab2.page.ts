d7bc8ffff7ff3dfe0b6d72a6443e219541782468
jhenriquecampos <jhcc1995@gmail.com>
2021-06-29 21:04:56 -0300

    Put the camera in tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,15 @@
+import { PhotoService } from './../services/photo.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  constructor(public photoService: PhotoService) {}
 
-  constructor() {}
-
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
